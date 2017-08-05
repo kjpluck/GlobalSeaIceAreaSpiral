0ca856fa448ba300a005a482e529157d649821e3
Kevin Pluck <kevinp@micad.co.uk>
2017-08-05 11:28:33 +0100

    removed makegif.bat

--- makegif.bat
+++
@@ -1,4 +0,0 @@
-
-
-ffmpeg -v warning -i processing-movie.mp4 -vf "palettegen" -y palette.png
-ffmpeg -v warning -i processing-movie.mp4 -i palette.png -lavfi "paletteuse" -y GlobalSeaIceAreaSpiral.gif
